2e581ddd0667512866f6a3a7dd36b7d4829c4f4d
Недислав Стойчев <nedislav.stoichev@gmail.com>
2017-05-21 21:36:57 +0200

    Added loading and drawing of level from files

--- LaserChess/LaserChess/ChessBoard/ChessBoard.cs
+++ LaserChess/LaserChess/ChessBoard/ChessBoard.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using LaserChess.Entities.AI;
+using LaserChess.Entities.Human;
+using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace LaserChess.ChessBoard
 {
 	public class ChessBoard
 	{
-		private const string _evenCellSymbol = "///";
-		private const string _oddCellSymbol = "|||";
+		private const string _evenCellSymbol = " * ";
+		private const string _oddCellSymbol = " # ";
+		private const int _rows = 8;
+		private const int _columns = 8;
 
-		private ChessBoardCell[] _chessBoardCells;
+		private ChessBoardCell[,] _chessBoardCells;
 
 		public int Rows { get; set; }
 
@@ -27,9 +28,15 @@ namespace LaserChess.ChessBoard
 			get { return _oddCellSymbol; }
 		}
 
-		public ChessBoardCell[] ChessBoardCells
+		public ChessBoardCell[,] ChessBoardCells
 		{
 			get { return _chessBoardCells; }
+		}
+
+		public ChessBoard()
+		{
+			Rows = _rows;
+			Columns = _columns;
 		}
 
 		/// <summary>
@@ -37,15 +44,116 @@ namespace LaserChess.ChessBoard
 		/// </summary>
 		public void Draw()
 		{
-			throw new NotImplementedException();
+			///("_ +___+___+___+___+___+___+___+___+");
+			///("  |   |   |   |   |   |   |   |   |");
+			///("A |   |   |   |   |   |   |   |   |");
+			///("  |___|___|___|___|___|___|___|___|");
+			///("  |   |   |   |   |   |   |   |   |");
+			///("B |   |   |   |   |   |   |   |   |");
+			///("  |___|___|___|___|___|___|___|___|");
+
+			Console.Clear();
+
+			Console.WriteLine("  +___+___+___+___+___+___+___+___+");
+
+			for (int row = 0; row < Rows; row++)
+			{
+				Console.WriteLine("  |   |   |   |   |   |   |   |   |");
+
+				var rowLine = new StringBuilder();
+				rowLine.Append($"{Rows - row} |");
+
+				bool invertEmptySpace = row % 2 != 0;
+				for (int column = 0; column < Columns; column++)
+				{
+					ChessBoardCell cell = ChessBoardCells[row, column];
+					if (cell.IsOccupied && cell.Entity != null)
+					{
+						rowLine.Append($"{cell}|");
+					}
+					else
+					{
+						if ((column % 2) == 0)
+						{
+							if (invertEmptySpace)
+							{
+								rowLine.Append($"{OddCellSymbol}|");
+							}
+							else
+							{
+								rowLine.Append($"{EvenCellSymbol}|");
+							}
+						}
+						else
+						{
+							if (invertEmptySpace)
+							{
+								rowLine.Append($"{EvenCellSymbol}|");
+							}
+							else
+							{
+								rowLine.Append($"{OddCellSymbol}|");
+							}
+						}
+					}
+				}
+
+				Console.WriteLine(rowLine.ToString());
+				Console.WriteLine("  |___|___|___|___|___|___|___|___|");
+			}
+
+			Console.WriteLine("  + A   B   C   D   E   F   G   H +");
 		}
 
 		/// <summary>
 		/// 
 		/// </summary>
-		public void LoadLevel(string levelPath)
+		public void LoadLevel(string[] levelPath)
 		{
-			throw new NotImplementedException();
+			_chessBoardCells = new ChessBoardCell[Rows, Columns];
+
+			for (int row = 0; row < Rows; row++)
+			{
+				for (int column = 0; column < Columns; column++)
+				{
+					char cell = (levelPath[row])[column];
+
+					var chessBoardCell = new ChessBoardCell();
+					switch(cell)
+					{
+						case 'G':
+							chessBoardCell.Entity = new Grunt();
+							chessBoardCell.IsOccupied = true;
+							break;
+						case 'J':
+							chessBoardCell.Entity = new Jumpship();
+							chessBoardCell.IsOccupied = true;
+							break;
+						case 'T':
+							chessBoardCell.Entity = new Tank();
+							chessBoardCell.IsOccupied = true;
+							break;
+						case 'D':
+							chessBoardCell.Entity = new Drone();
+							chessBoardCell.IsOccupied = true;
+							break;
+						case 'N':
+							chessBoardCell.Entity = new Dreadnought();
+							chessBoardCell.IsOccupied = true;
+							break;
+						case 'C':
+							chessBoardCell.Entity = new CommandUnit();
+							chessBoardCell.IsOccupied = true;
+							break;
+						case '0':
+							chessBoardCell.Entity = null;
+							chessBoardCell.IsOccupied = false;
+							break;
+					}
+
+					_chessBoardCells[row, column] = chessBoardCell;
+				}
+			}
 		}
 	}
 }
--- LaserChess/LaserChess/LaserChess.cs
+++ LaserChess/LaserChess/LaserChess.cs
@@ -1,17 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace LaserChess
 {
-    class LaserChess
-    {
-        static void Main(string[] args)
-        {
+	class LaserChess
+	{
+		private const string _quitCommand = "exit";
 
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Environment.NewLine);
+			Console.WriteLine("Welcome to LaserChess!");
+			Console.WriteLine(Environment.NewLine);
 
-        }
-    }
+			//TODO: Get level count from the 'Levels' directory. Read the level names from thier file names.
+			Console.WriteLine("Choose Chess board:");
+			Console.WriteLine("1. Initial fight");
+			Console.WriteLine("2. WTF?!");
+			Console.WriteLine("3. Dreadnought me baby!");
+			Console.WriteLine("or write 'exit' to quit LaserChess!");
+			Console.WriteLine(Environment.NewLine);
+
+			int levelPick = 0;
+			while (levelPick == 0)
+			{
+				Console.Write("Select board (1, 2 or 3) or quit: ");
+
+				string line = Console.ReadLine();
+				if (line == _quitCommand)
+				{
+					return;
+				}
+
+				int userPick = 0;
+				bool validInput = Int32.TryParse(line, out userPick);
+
+				if (validInput)
+				{
+					if ((userPick > 0) && (userPick < 4))
+					{
+						levelPick = userPick;
+					}
+					else
+					{
+						Console.WriteLine($"'{line}' is not valid chess board. Please pick one of the specified chess boards.");
+					}
+				}
+				else
+				{
+					Console.WriteLine($"'{line}' is not valid chess board. Please pick one of the specified chess boards.");
+				}
+			}
+
+			string[] rawChessBoard;
+			try
+			{
+				rawChessBoard = LoadLevelFromFile(levelPick);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				Console.ReadLine();
+				return;
+			}
+
+			var chessBoard = new ChessBoard.ChessBoard();
+			chessBoard.LoadLevel(rawChessBoard);
+
+			chessBoard.Draw();
+
+			Console.WriteLine("End of program...");
+			Console.ReadLine();
+		}
+
+		private static string[] LoadLevelFromFile(int levelPick)
+		{
+			var level = new FileInfo($"{Environment.CurrentDirectory}\\Levels\\Level{levelPick}.txt");
+
+			string rawChessBoard = level.OpenText().ReadToEnd();
+			rawChessBoard = rawChessBoard.Replace(Environment.NewLine, "|");
+
+			if (rawChessBoard.Length != 71)
+			{
+				throw new FileLoadException($"Invalid level: {rawChessBoard}");
+			}
+
+			string[] chessBoard = rawChessBoard.Split('|');
+
+			return chessBoard;
+		}
+
+		private static void DrawLevel(string[] chessBoard)
+		{
+		}
+	}
 }
